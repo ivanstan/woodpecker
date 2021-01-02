072d815f659afa9241bafdf609e08982dc684b57
Ivan Stanojevic <ivan.stanojevic+github@protonmail.com>
2021-01-02 20:54:52 +0100

    Create Url.tsx

---
+++ react/Url.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+https://github.com/ivanstan/woodpecker
+interface UrlPropsInterface {
+  href?: string;
+  external?: boolean;
+}
+
+export class Url extends React.Component<UrlPropsInterface, any> {
+  render() {
+    const attributes: any = {};
+
+    if (this.props.external) {
+      attributes.target = "_blank";
+      attributes.rel = "nofollow noopener";
+    }
+
+    const href = this.props.href || this.props.children;
+
+    return <a href={href} {...attributes}>
+      {this.props.children}
+    </a>;
+  }
+}
